6ed8acfa656e94bcdc74592d64a772105bd4d132
Neo <c.min.tseng@gmail.com>
2016-12-02 20:03:01 +0800

    First commit : Change to Fragment

---
+++ README.md
@@ -0,0 +1,9 @@
+Refactoring Synnix Contactless Smart Card Reader Sample Code App
+
+1. Activity to Fragment
+
+2. use MVC
+
+3. use Design Patterns
+
+4. Refactoring
---
+++ app/src/main/java/com/example/smartcardapp/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.smartcardapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+    //TestActivity rename to MainActivity
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+}
